6bc5b7dd1c953c8c8969f4d890797b3234e57248
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-12-09 15:04:46 +0530

    Added Navbar

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("Navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
 window.onload = function () {
     // or enable responsive functionality:
     GitHubCalendar(".calendar", "kirankumar-medikurthy", { responsive: true });
